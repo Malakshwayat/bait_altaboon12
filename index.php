92acfb8d006d8d444d93c8720d0b7d18b85c176e
Malakshwayat <malakshcs98@gmail.com>
2025-01-08 14:09:11 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
-
-<htmL>
-<head>    
-<link rel="stylesheet" href="./assets/styles/index.css"> 
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="./assets/style/style1.css">
 </head>
 <body>
-<?php
-require 'navabar.php';?>
-<div class="container">
-<div class="mouse">
-<video class="vd" autoplay loop muted>
-<source src="./assets/pix/leen.mp4"type="video/mp4">
-</video></div>
-<div class="description">
-    <h2>!مرحبًا بكم في بيت الطابون</h2>
-    <p>
-    نحن نقدم لكم تجربة فريدة من نوعها مع أشهى الأطباق التقليدية
-        التي تعكس تراثنا الغني.
-        انضم إلينا للاستمتاع بأجواء دافئة وخدمة مميزة.
-    </p>
+<?php require 'sidbar.php'; ?>
+<div class="content ">
+<h1>Malak shwayat</h1>
+<h2> computer Science<br> Balqa Applied University - Al Huson College 2017-2021<br>
+Bachelor of Computer Science | GPA : 2.97 </h2><br>
+<div class="img1 row ">
+<img src="./assets/pix/e7a9797d-f8ae-40ec-be0b-eda1be95aad1.jpg"class="col-md-4">
+<img src="./assets/pix/83b36846-2ed6-4f8c-a2be-94d089af3bab.jpg"   class="col-md-4">
+<img src="./assets/pix/07cf28ee-3f0e-4696-afd1-c3e5a6dbfd03.jpg"  class="col-md-4">
+<img src="./assets/pix/563aedff-f144-480a-a708-b1465456d7ad.jpg"class="col-md-4">
+<img src="./assets/pix/a0dc88b5-b455-482c-83cc-7ca7acf56c84.jpg"   class="col-md-4">
+<img src="./assets/pix/fb16a302-a93a-4305-88d1-b414c73da7fd.jpg"  class="col-md-4">
+<img src="./assets/pix/1705434127678.jpg"  class="col-md-4">
+<img src="./assets/pix/17bdaca0-e39d-43b8-a798-e99033264920.jpg"  class="col-md-4">
+<img src="./assets/pix/81e9658a-b69f-499b-8c55-5515b57e6524.jpg"  class="col-md-4">
 </div>
-</div>
-<?php require 'footer.php';?> 
+</div> 
 </body>
-</html>
+</htmL>
